33374c9ad8ace4bd8ae043316f4300d71e864684
Dmitriy Makarov <makarov.dmitriy@gmail.com>
2017-07-10 11:13:39 +0300

    The simple counter implementation

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="counter">
+        <p>{{ count }}</p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'counter',
+    data () {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increment () {
+        this.count++
+      },
+      decrement () {
+        this.count--
+      }
+    }
+  }
+</script>
